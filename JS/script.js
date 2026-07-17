060957aafa7e6294b1c7856c2ca45a28f44b262b
8anissibrenda-stack <8anissi.brenda@gmail.com>
2026-07-17 13:28:57 +0530

    done

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,21 @@
 //TIMELINE ANIMATION
 const timelineItems = document.querySelectorAll(".timeline-item");
 const observer = new IntersectionObserver(
-(entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
-            const index = [...timelineItems].indexOf(entry.target);
-            setTimeout(()=>{
-                entry.target.classList.add("show");
-            }, index * 180);
-        }
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const index = [...timelineItems].indexOf(entry.target);
+        setTimeout(() => {
+          entry.target.classList.add("show");
+        }, index * 180);
+      }
     });
-},
-{
-    threshold:0.3
-});
-timelineItems.forEach((item)=>{
-    observer.observe(item);
+  },
+  {
+    threshold: 0.3
+  }
+);
+
+timelineItems.forEach((item) => {
+  observer.observe(item);
 });
